7747af5592cfd829f59214a755dd5c32fd95c6f2
tuff <t.ghostersons+git@gmail.com>
2019-09-27 10:24:05 -0700

    bump savior version

--- dist/ao3-savior.user.js
+++ dist/ao3-savior.user.js
@@ -4,7 +4,7 @@
 // @namespace     ao3
 // @include       http*://archiveofourown.org/*
 // @grant         none
-// @version       1.12
+// @version       1.13
 // @downloadURL   https://github.com/tuff/tuff-userscripts/raw/master/dist/ao3-savior.user.js
 // ==/UserScript==
 
